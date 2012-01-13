67a4ec393e6d1242e9eb8d20f555fbd48012caca
Desmond Smith <des@webandflowdesign.com>
2012-01-12 23:09:38 -0500

    Changes to allow loading of Google CDN jQuery and reference MODX core path settings

--- snippets/snippet.basicmenu.php
+++ snippets/snippet.basicmenu.php
@@ -1,5 +1,8 @@
 <?php
-require_once('core/components/basicmenu/classes/class.basicmenu.php');
+/* Get the MODX Core path, and set the source path for BasicMenu */
+$core = $modx->getOption('core_path');
+$basicMenuSource  = $core . 'components/basicmenu/classes/class.basicmenu.php'; 
+require_once($basicMenuSource);
 
 $menu			= new BasicMenu(0,$modx);
 $output			= '';
@@ -7,20 +10,33 @@ $output			= '';
 $resource               = $modx->resource;
 $id=$resource->get('id');
 
-// Check need to register HoverIntent and Default CSS
+/*
+  * Check the options:
+  * jQuery
+  * HoverIntent
+  * Default CSS
+*/
+
+($loadJQuery) ? $modx->regClientStartupScript('https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js') : '';
+
 if ($useHoverIntent == 1) {
-	$modx->regClientScript('core/components/basicmenu/js/hoverintent/jquery.hoverIntent.minified.js');
-	$modx->regClientScript('core/components/basicmenu/js/hoverintent/jquery.hoverintent.config.js');
+
+
+	$modx->regClientScript($core . 'components/basicmenu/js/hoverintent/jquery.hoverIntent.minified.js');
+	$modx->regClientScript($core . 'components/basicmenu/js/hoverintent/jquery.hoverintent.config.js');
 }
 
 $output .= ($useDefaultCSS == 1) ? $modx->getChunk('basicMenuDefaultCSS') : '';
 
 // Process the Menu
-$output			.= $menu->showMenu();
+$data = array();
+$data['menu']			.= $menu->showMenu();
 
+$output .= $modx->getChunk('basicMenuWrapper',$data);
 
 if ($id != $modx->getOption('site_unavailable_page')) {
 return $output;
 } else {
 return '';
 }
+?>
